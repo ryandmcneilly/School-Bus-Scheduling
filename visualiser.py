59552ef671e155fdd3a1c77ba7ef0e955af9c3dc
ryandmcneilly <ryanmcn@live.com.au>
2024-10-07 15:08:53 +1000

    Fixed visualiser

--- visualiser.py
+++ visualiser.py
@@ -1,17 +1,29 @@
 from pyvis.network import Network
+from reportImplementation import *  # Assuming this imports necessary data and variables
 
-from reportImplementation import *
-
-# Need some trip id, value (colour based on degree), title (id), x, y, label, colour (z)
-
-
-# 1 = FF0000
+# Initialize network object
 net = Network()
-net.add_nodes(range(1, len(N) + 1), value=[sum([X[i, j, k].x for j in N for k in K]) for i in N ], 
-              title=range(1, len(N) + 1), 
-              x=[SCHOOL_POSITIONS[i][0] for i in N], 
-              y=[SCHOOL_POSITIONS[i][1] for i in N], 
-              label=[f"School {i}" for i in N], #color=[f"rgb({sum([Z[i, j, LATE].x for j in N] for i in N) / 255}, 00, {sum([Z[i, j, EARLY].x for j in N] for i in N) / 255})"])
-)
 
+# Create nodes for the network based on the 'Z' and 'X' variables
+for i in N:
+    # Sum up the X[i,j,k] values to determine the size of the node (value)
+    node_value = sum([X[i, j, k].x for j in N for k in K])
+
+    # red = int(min(sum([Z[i, j, EARLY].x for j in N]), 255))
+    # blue = int(min(sum([Z[i, j, LATE].x for j in N]), 255))
+    # color = f"rgb({red}, 0, {blue})"
+
+    # Add each node with respective attributes
+    net.add_node(f"{i}", value=int(node_value), title=f"School {int(i)}", 
+                 x=float(SCHOOL_POSITIONS[i][0]), y=float(SCHOOL_POSITIONS[i][1]),
+                 label=f"School {i}", #color=color)
+    )
+
+
+for i in N:
+    for j in N:
+        for k in N:
+            if X[i, j, k].x > 0:
+                net.add_edge(str(i), str(j))
+# Display the network
 net.show('network.html')
