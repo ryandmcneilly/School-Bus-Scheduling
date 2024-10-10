e69b9d33bc768dc3f1acd456ac37127d42521d85
ryandmcneilly <ryanmcn@live.com.au>
2024-10-10 18:06:08 +1000

    Visualiser fixed, not plots depot

--- visualiser.py
+++ visualiser.py
@@ -1,23 +1,14 @@
 from pyvis.network import Network
 from main import *  # Assuming this imports necessary data and variables
 
-# Initialize network object
 net = Network(directed=True)
 
-# Create nodes for the network based on the 'Z' and 'X' variables
+net.add_node(f"0", color="#808080", title="Bus Depot", x=DEPOT_POSITION[0], y=DEPOT_POSITION[1])
+
 for i in N:
-    # Sum up the X[i,j,k] values to determine the size of the node (value)
-    node_value = sum([X[i, j, k].x for j in N for k in K])
-
-    # red = int(min(sum([Z[i, j, EARLY].x for j in N]), 255))
-    # blue = int(min(sum([Z[i, j, LATE].x for j in N]), 255))
-    # color = f"rgb({red}, 0, {blue})"
-
-    # Add each node with respective attributes
-    net.add_node(f"{i}", value=int(node_value), title=f"School {int(i)}", 
+    net.add_node(f"{i}", title=f"School {int(i)}", 
                  x=float(SCHOOL_POSITIONS[i][0]), y=float(SCHOOL_POSITIONS[i][1]),
-                 label=f"School {i}", #color=color)
-    )
+                 label=f"School {i}")
 
 for i in N:
     for j in N:
